2102e45d345e3063cc5bb9aea108458ce1385cdd
Logan Cole <emtlogancole@gmail.com>
2022-12-06 18:57:39 -0600

    Add code

--- friends.rb
+++ friends.rb
@@ -1,7 +1,12 @@
 best_friend = ['John']
 name = 'Josh'
+name = 'Jack'
 if name == 'John'
     puts 'Hello, John'
+elsif name == 'Jack'
+    puts 'Whats up Jack?!'
 else 
     puts 'Hello, Stranger'
 end
+
+name2 = 'Jack'
